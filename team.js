610ac1b12fc352add8e40791e601361cfc91dc6b
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-04-13 20:46:46 +0200

    funzione aggiunta

--- team.js
+++ team.js
@@ -60,3 +60,24 @@ const team = [{
     petName: "Tofa",
 },
 ];
+
+//funzione trova gioco
+function findGame(array){
+    for(let i=0; i < array.length; i++){
+        if(array[i].favoriteVideoGame === "LoL" || array[i].favoriteVideoGame === "League of Legends" || array[i].favoriteVideoGame === "League of legends"){
+            console.log(array[i].name);
+        }
+    }
+}
+
+findGame(team)
+
+// funzione per stampare i nomi in ordine alfabetico
+
+function printTeam(array) {
+    array.sort((a, b) => (a.surname > b.surname) ? 1 : -1);
+    for (let i = 0; i < array.length; i++) {
+        console.log(`${array[i].surname} ${array[i].name}`);
+    }
+}
+printTeam(team)
